435e8f4324b8653f7704522c57b09e5640054371
olex-bel <ban_konung@ukr.net>
2024-08-03 21:47:30 +0200

    Added optimistic UI

--- app/components/ToDoItem.tsx
+++ app/components/ToDoItem.tsx
@@ -1,8 +1,8 @@
 
 import { useFetcher, Link  } from "@remix-run/react"
 import { RiDeleteBin2Line, RiEdit2Line } from "@remixicon/react";
+import TodoItemTitle from "./TodoItemTitle";
 import type { ToDo } from "~/utils/todo.operations.server";
-import { ChangeEvent } from "react";
 
 type ToDoItemProps = {
     item: ToDo;
@@ -10,25 +10,10 @@ type ToDoItemProps = {
 
 export default function TodoItem({ item }: ToDoItemProps) {
     const fetcher = useFetcher();
-    const handleCompletionFlagChange = (event: ChangeEvent<HTMLInputElement>) => {
-        event.preventDefault();
-        fetcher.submit(event.target.form);
-    };
 
     return (
         <li className="flex gap-2 rounded cursor-pointer px-4 py-2 w-full bg-slate-100 even:bg-slate-200">
-            <fetcher.Form method="post" action={`todo/${item.id}/toggle-complete`} className="grow peer">
-                <label className="has-[:checked]:line-through has-[:checked]:text-slate-500">
-                    <input 
-                        type="checkbox" 
-                        name="completed" 
-                        checked={item.completed? true : false} 
-                        className="mr-2" 
-                        onChange={handleCompletionFlagChange}
-                    />
-                    {item.title}
-                </label>
-            </fetcher.Form>
+            <TodoItemTitle id={item.id} title={item.title} completed={item.completed} />
             <fetcher.Form method="post" action={`todo/${item.id}/delete`}>
                 <button 
                     name="_action"
